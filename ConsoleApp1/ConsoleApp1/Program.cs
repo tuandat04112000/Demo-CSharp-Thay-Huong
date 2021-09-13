09143597e09a184189f291a71aef75a708142d3a
Huỳnh Tấn Phúc <63900676+Phuc-code-2001@users.noreply.github.com>
2021-09-13 21:09:35 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+ 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static protected string s ="Hello World";// demo Access specifier
+        static void Main(string[] args)
+        {
+            Car myO = new Car();
+            //Console.WriteLine(myO.model);
+            myO.nameCar(); // demo phần kế thừa
+
+                Animal myAnimal = new Animal();  // Create a Animal object
+                Animal myPig = new Pig();  // Create a Pig object
+                Animal myDog = new Dog();  // Create a Dog object
+
+                //demo phần bao đóng
+
+                myAnimal.animalSound();
+                myPig.animalSound();
+                myDog.animalSound();
+        }
+    }
+    class Car : Program
+    {
+        protected string model = "Ferrari";
+
+        public void nameCar(){
+            Console.WriteLine(s);
+        }
+    }
+
+    class Animal  // Base class (parent) 
+{
+  public virtual void animalSound() 
+  {
+    Console.WriteLine("The animal makes a sound");
+  }
+}
+
+class Pig : Animal  // Derived class (child) 
+{
+  public override void animalSound() 
+  {
+    Console.WriteLine("The pig says: wee wee");
+  }
+}
+
+class Dog : Animal  // Derived class (child) 
+{
+  public override void animalSound() 
+  {
+    Console.WriteLine("The dog says: bow wow");
+  }
+}
+}
